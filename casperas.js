acc737b4227e3fc0b1e7452d3bc9ce45c36b2256
Specialistik <test04@list.ru>
2018-12-23 18:09:06 +0300

    like response is being printed

--- casperas.js
+++ casperas.js
@@ -10,11 +10,18 @@ casper.start("https://www.business-gazeta.ru/article/406831", function() {
 
     casper.thenEvaluate(function() {
         this.click('input[id="radio-variant-f164-148252"][value="yes"]');
-        casper.wait(4000, function(){echo('2')});
-        response = document.querySelector('input[type="submit"]').click();
+        //casper.wait(4000, function(){echo('2')});
+        response = document.querySelector("form[action='/polls/vote']").click();
         console.log(response);
+
+        
         //document.querySelector('input[id="radio-variant-f164-148252"]').setAttribute('selected', true);
     });
+
+    casper.then(function () {
+        console.log('clicked ok, new location is ' + this.getCurrentUrl());
+    });
+    casper.run();
 
 /*
     casper.thenEvaluate(
@@ -25,8 +32,9 @@ casper.start("https://www.business-gazeta.ru/article/406831", function() {
     });
     */
 });
-
+/*
 casper.run(function() {
     console.log(this.getCurrentUrl(),'info'); 
     this.exit();
-  });
+});
+*/
