1453459f112a2867a8def0e90b3d8e1c58a86d10
Specialistik <test04@list.ru>
2018-12-23 17:21:53 +0300

    casper thingies

--- casperas.js
+++ casperas.js
@@ -5,14 +5,6 @@ casper.start("https://www.business-gazeta.ru/article/406831", function() {
         //document.querySelector('input[id="radio-variant-f164-148252"]').setAttribute('selected', true);
     });
 
-    document.querySelector(
-//searches and fills the form with id="loginForm"
-/*
-  this.fill('form#loginForm', {
-    'login':    'admin',
-    'password':    '12345678'
-   }, true);
-*/
   this.evaluate(function(){
     //trigger click event on submit button
     document.querySelector('input[type="submit"]').click();
