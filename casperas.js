6a716906671d3c39dc523b8484bf5cffdc21934c
Specialistik <test04@list.ru>
2018-12-23 17:35:18 +0300

    casper real things

--- casperas.js
+++ casperas.js
@@ -1,15 +1,25 @@
+var casper = require('casper').create({
+    verbose: true,
+    logLevel: 'debug'
+});
+
+
 casper.start("https://www.business-gazeta.ru/article/406831", function() {
+    console.log('voting page loaded');
     this.waitForSelector('form[action="/polls/vote"]');
-});
 
-casper.thenEvaluate(function() {
-    this.click('input[id="radio-variant-f164-148252"][value="yes"]');
-    //document.querySelector('input[id="radio-variant-f164-148252"]').setAttribute('selected', true);
-});
-
-casper.thenEvaluate(function(){
-//trigger click event on submit button
-    document.querySelector('input[type="submit"]').click();
+    casper.thenEvaluate(function() {
+        this.click('input[id="radio-variant-f164-148252"][value="yes"]');
+        casper.wait(4000, function(){echo('2')});
+        document.querySelector('input[type="submit"]').click();
+        //document.querySelector('input[id="radio-variant-f164-148252"]').setAttribute('selected', true);
+    });
+/*
+    casper.thenEvaluate(function(){
+    //trigger click event on submit button
+        document.querySelector('input[type="submit"]').click();
+    });
+    */
 });
 
 casper.run();
