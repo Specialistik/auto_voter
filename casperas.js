69260bee6761b4ea6c3f00fd8891a45a1f5f0386
Specialistik <test04@list.ru>
2018-12-23 17:25:31 +0300

    like nice casper code

--- casperas.js
+++ casperas.js
@@ -1,12 +1,13 @@
 casper.start("https://www.business-gazeta.ru/article/406831", function() {
+    this.waitForSelector('form[action="/polls/vote"]');
+});
 
-    casper.thenEvaluate(function(term) {
-        this.click('input[id="radio-variant-f164-148252"][value="yes"]');
-        //document.querySelector('input[id="radio-variant-f164-148252"]').setAttribute('selected', true);
-    });
+casper.thenEvaluate(function() {
+    this.click('input[id="radio-variant-f164-148252"][value="yes"]');
+    //document.querySelector('input[id="radio-variant-f164-148252"]').setAttribute('selected', true);
+});
 
-  this.evaluate(function(){
-    //trigger click event on submit button
+casper.thenEvaluate(function(){
+//trigger click event on submit button
     document.querySelector('input[type="submit"]').click();
-  });
 });
