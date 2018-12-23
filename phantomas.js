d8686311b9447975eca015aee9d2f97e0168a627
Specialistik <test04@list.ru>
2018-12-23 17:00:25 +0300

    phantomjs things

--- phantomas.js
+++ phantomas.js
@@ -2,6 +2,7 @@ var page = require('webpage').create();
 console.log('The default user agent is ' + page.settings.userAgent);
 page.settings.userAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
 page.open('https://www.business-gazeta.ru/article/406831', function(status) {
+    console.log('like things began to happen');
   if (status !== 'success') {
     console.log('Unable to access network');
   } else {
@@ -11,6 +12,7 @@ page.open('https://www.business-gazeta.ru/article/406831', function(status) {
        console.log(forms[0]);
        console.log(forms[1]);
        console.log(forms[2]);
+       return;
        //document.getElementsByClassName("login-form");
     });
     console.log(ua);
